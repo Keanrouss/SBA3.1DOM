ed66a2db699b5b28164a0147073080494d5b5480
keanrouss <timajori1@gmail.com>
2025-05-23 14:45:06 -0400

    fifth commit

---
+++ SBA3.1.js
@@ -0,0 +1,5 @@
+//selecting all of the buttons elements
+let buttonEl = document.querySelectorAll("button");
+//selecting inputs by their ID/ narrowing down "output" bar to show result
+let inputEl = document.getElementById('Output');
+// to be able to run through everything 
