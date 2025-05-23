e33fdb4c7dc4c8afd2fa57236fc7795e94249da9
keanrouss <timajori1@gmail.com>
2025-05-23 15:43:26 -0400

    seventh commit

--- SBA3.1.js
+++ SBA3.1.js
@@ -1,7 +1,7 @@
 //selecting all of the buttons elements
 let CalcbuttonsEl = document.querySelectorAll("button");
 //selecting inputs by their ID/ narrowing down "output" bar to show result
-let inputEl = document.getElementById('Output');
+let inputfieldEl = document.getElementById('Output');
 // to be able to run through everything/ only loop I am able to perform confidently
 for(let i=0; i< CalcbuttonsEl.length; i++){
     CalcbuttonsEl[i].addEventListener("click", () => {
@@ -21,8 +21,11 @@ if (buttonValue === "C"){
 // to get the function to calculate whatever amount is clicked.
 // "" space meaning..infinite opening as the numbers calculated is unknown
 function calculateOutput() {
-    inputEl.value ="";  
+    inputfieldEl.value =//?  
 }
-
+ function clearOutput() {
+    inputfieldEl.value ="";
+ }
+    
 
 
