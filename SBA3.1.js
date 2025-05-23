124b4e4936c322b14cf70e6cb5aa0dbf7920e722
keanrouss <timajori1@gmail.com>
2025-05-23 17:19:14 -0400

    eigth commit

--- SBA3.1.js
+++ SBA3.1.js
@@ -3,29 +3,20 @@ let CalcbuttonsEl = document.querySelectorAll("button");
 //selecting inputs by their ID/ narrowing down "output" bar to show result
 let inputfieldEl = document.getElementById('Output');
 // to be able to run through everything/ only loop I am able to perform confidently
-for(let i=0; i< CalcbuttonsEl.length; i++){
-    CalcbuttonsEl[i].addEventListener("click", () => {
+for (let i = 0; i < CalcbuttonsEl.length; i++) {
+    CalcbuttonsEl[i].addEventListener("click", (e) => {
         //assigning "C" button value role on the calculator as an eraser
         // while assigning "=" button value role on the calculator output display
-const buttonValue = CalcbuttonsEl[i].textContent;
-
-if (buttonValue === "C"){
-    clearOutput();
-}else if (buttonValue === "="){
-    calculateOutput();
-    } else {
-        appendValue(buttonValue)
-    }
-});
+        const buttonValue = CalcbuttonsEl[i].textContent;
+        console.log(buttonValue)
+        if (buttonValue === "C") {
+            inputfieldEl.value = "";
+        } else if (buttonValue === "=") {
+            inputfieldEl.value = eval(inputfieldEl.value)//?
+        } else {
+            inputfieldEl.value+= buttonValue
+        }
+    });
 }
 // to get the function to calculate whatever amount is clicked.
 // "" space meaning..infinite opening as the numbers calculated is unknown
-function calculateOutput() {
-    inputfieldEl.value =//?  
-}
- function clearOutput() {
-    inputfieldEl.value ="";
- }
-    
-
-
